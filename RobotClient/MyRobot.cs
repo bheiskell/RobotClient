a3a9c1a99a8f4f65d5eb40e85580e026dccb6f25
William Speirs <bill.speirs@gmail.com>
2012-11-06 10:24:54 -0500

    Init import of project

---
+++ RobotClient/MyRobot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GameClient;
+using GameClient.Objects;
+
+namespace CSharpLabClient
+{
+   // TODO: 1.) Define a new public class called MyRobot deriving from BasePlayer
+   // TODO: 2.) Create a public parameterless constructor for the class
+   // TODO: 3.) Implement 'Think' function to contain logic
+
+}
